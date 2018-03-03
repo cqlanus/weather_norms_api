1437da28290deb5d9cbaea3694a5adbae5a7614e
Chris Lanus <cqlanus@gmail.com>
2018-03-03 14:52:56 -0600

    updating cors settings

--- server/index.js
+++ server/index.js
@@ -20,7 +20,22 @@ passport.deserializeUser((id, done) => {
         .catch(done);
 });
 
+var allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 const createApp = () => {
+    app.use(allowCrossDomain)
     app.use(morgan('dev'));
 
     app.use(bodyParser.json());
